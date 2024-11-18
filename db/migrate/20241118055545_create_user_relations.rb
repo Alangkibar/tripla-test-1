80ca2d8ddae4699d6da2f0b77b1e4ae058856c76
Ahmad Alangkibar <alangkibar25@gmail.com>
2024-11-18 12:59:57 +0700

    feat: create initial migration

---
+++ db/migrate/20241118055545_create_user_relations.rb
@@ -0,0 +1,13 @@
+class CreateUserRelations < ActiveRecord::Migration[8.0]
+  def change
+    create_table :user_followings do |t|
+      t.references :follower, index: true
+      t.references :followed, index: true
+
+      t.timestamps
+    end
+
+    add_foreign_key :user_followings, :users, column: :follower_id
+    add_foreign_key :user_followings, :users, column: :followed_id
+  end
+end
